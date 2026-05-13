45daa88512c58fd4a016852a3780867d23024ce9
paulomussi <127447854+paulomussi@users.noreply.github.com>
2026-05-13 20:19:40 -0300

    mer

--- src/controllers/atribuicaoTarefas.js
+++ src/controllers/atribuicaoTarefas.js
@@ -1,155 +1,136 @@
-const db = require('../dataBase/connection');
+const db = require("../dataBase/connection");
 
 module.exports = {
-    async listarAtribuicaoTarefas(request, response) {
-        try {
-
-            const sql = `
+  async listarAtribuicaoTarefas(request, response) {
+    try {
+      const sql = `
             SELECT 
                 atr_id, atr_tarefa_id, atr_funcionario_id, atr_status, atr_data_atribuicao 
             FROM ATRIBUICAO_TAREFAS;
             `;
 
-            const [atribuicaoTarefas] = await db.query(sql);
+      const [atribuicaoTarefas] = await db.query(sql);
 
+      return response.status(200).json({
+        sucesso: true,
+        mensagem: "Lista de atribuição de tarefas feita com sucesso",
+        itens: atribuicaoTarefas.length,
+        dados: atribuicaoTarefas,
+      });
+    } catch (error) {
+      return response.status(500).json({
+        sucesso: false,
+        mensagem: `erro ao listar atribuição de tarefas: ${error.message} `,
+        dados: null,
+      });
+    }
+  },
+  async cadastrarAtribuicaoTarefas(request, response) {
+    try {
+      const { status, tarefaId, funcId } = request.body;
 
-            return response.status(200).json(
-                {
-                    sucesso: true,
-                    mensagem: 'Lista de atribuição de tarefas feita com sucesso',
-                    itens: atribuicaoTarefas.length,
-                    dados: atribuicaoTarefas
-                }
-            );
-        } catch (error) {
-            return response.status(500).json(
-                {
-                    sucesso: false,
-                    mensagem: `erro ao listar atribuição de tarefas: ${error.message} `,
-                    dados: null
-                }
-            );
-        }
-    },
-    async cadastrarAtribuicaoTarefas(request, response) {
-        try {
-            const { status, tarefaId, funcId } = request.body;
-
-            const sql = `INSERT INTO ATRIBUICAO_TAREFAS 
+      const sql = `INSERT INTO ATRIBUICAO_TAREFAS 
                 (atr_tarefa_id, atr_funcionario_id, atr_status, atr_data_atribuicao)
             VALUES
                 (?, ?, ?, NOW());`;
 
-            const values = [tarefaId, funcId, status];
+      const values = [tarefaId, funcId, status];
 
-            const [result] = await db.query(sql, values);
+      const [result] = await db.query(sql, values);
 
-            const dados = {
-                id: result.insertId
-            };
+      const dados = {
+        id: result.insertId,
+      };
 
-            return response.status(200).json(
-                {
-                    sucesso: true,
-                    mensagem: 'Cadastro de tarefas feita com sucesso',
-                    dados: dados
-                }
-            );
-        } catch (error) {
-            return response.status(500).json(
-                {
-                    sucesso: false,
-                    mensagem: `erro ao cadastrar tarefas.`,
-                    dados: error.message
-                }
-            );
-        }
-    },
-    async editarAtribuicaoTarefas(request, response) {
-        try {
+      return response.status(200).json({
+        sucesso: true,
+        mensagem: "Cadastro de tarefas feita com sucesso",
+        dados: dados,
+      });
+    } catch (error) {
+      return response.status(500).json({
+        sucesso: false,
+        mensagem: `erro ao cadastrar tarefas.`,
+        dados: error.message,
+      });
+    }
+  },
+  async editarAtribuicaoTarefas(request, response) {
+    try {
+      const { status, data_atribuicao, atr_funcionario } = request.body;
 
-            const { status, data_atribuicao } = request.body;
+      const { id } = request.params;
 
-            const { id } = request.params;
-
-            const sql = `
+      const sql = `
                 UPDATE atribuicao_tarefas SET 
                     atr_status = ?,    
-                    atr_data_atribuicao = ?
+                    atr_data_atribuicao = ?,
+                    atr_funcionario_id = ?
                 WHERE 
                     atr_id = ?
             `;
 
-            const values = [status, data_atribuicao, id];
+      const values = [status, data_atribuicao, atr_funcionario, id];
 
-            const [result] = await db.query(sql, values);
+      const [result] = await db.query(sql, values);
 
-            if (result.affectedRows === 0) {
-                return response.status(404).json({
-                    sucesso: false,
-                    mensagem: `Atribuição de tarefas ${id} não encontrado!`,
-                    dados: null
-                });
-            }
+      if (result.affectedRows === 0) {
+        return response.status(404).json({
+          sucesso: false,
+          mensagem: `Atribuição de tarefas ${id} não encontrado!`,
+          dados: null,
+        });
+      }
 
-            const dados = {
-                id,
-                status,
-                data_atribuicao
+      const dados = {
+        id,
+        status,
+        data_atribuicao,
+        atr_funcionario,
+      };
 
-            };
+      return response.status(200).json({
+        sucesso: true,
+        mensagem: "Editar atribuição de tarefas feita com sucesso",
+        dados: null,
+      });
+    } catch (error) {
+      return response.status(500).json({
+        sucesso: false,
+        mensagem: `erro ao editar atribuição de tarefas: ${error.message} `,
+        dados: null,
+      });
+    }
+  },
+  async apagarAtribuicaoTarefas(request, response) {
+    try {
+      const { id } = request.params;
 
-            return response.status(200).json(
-                {
-                    sucesso: true,
-                    mensagem: 'Editar atribuição de tarefas feita com sucesso',
-                    dados: null
-                }
-            );
-        } catch (error) {
-            return response.status(500).json(
-                {
-                    sucesso: false,
-                    mensagem: `erro ao editar atribuição de tarefas: ${error.message} `,
-                    dados: null
-                }
-            );
-        }
-    },
-    async apagarAtribuicaoTarefas(request, response) {
-        try {
-
-            const { id } = request.params;
-
-            const sql = `
+      const sql = `
                 DELETE FROM atribuicao_tarefas
                 WHERE atr_id = ?
                 `;
 
-            const values = [id];
-            const [result] = await db.query(sql, [values]);
+      const values = [id];
+      const [result] = await db.query(sql, [values]);
 
-            if (result.affectedRows === 0) {
-                return response.status(404).json({
-                    sucesso: false,
-                    mensagem: `Atribuição ${id} não encontrada`
-                });
-            }
-            return response.status(200).json(
-                {
-                    sucesso: true,
-                    mensagem: 'Apagar atribuição de tarefas feito com sucesso',
-                    dados: null
-                }
-            );
-        } catch (error) {
-            return response.status(500).json(
-                {
-                    sucesso: false,
-                    mensagem: `erro ao apagar atribuição de tarefas: ${error.message} `,
-                    dados: null
-                }
-            );
-        }
-    },
-}
+      if (result.affectedRows === 0) {
+        return response.status(404).json({
+          sucesso: false,
+          mensagem: `Atribuição ${id} não encontrada`,
+        });
+      }
+      return response.status(200).json({
+        sucesso: true,
+        mensagem: "Apagar atribuição de tarefas feito com sucesso",
+        dados: null,
+      });
+    } catch (error) {
+      return response.status(500).json({
+        sucesso: false,
+        mensagem: `erro ao apagar atribuição de tarefas: ${error.message} `,
+        dados: null,
+      });
+    }
+  },
+};
