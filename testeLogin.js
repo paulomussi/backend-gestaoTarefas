61e94680e3529820e4ab1ba36de69a110f0d15de
paulomussi <127447854+paulomussi@users.noreply.github.com>
2026-05-15 22:46:47 -0300

    d

--- testeLogin.js
+++ testeLogin.js
@@ -2,8 +2,7 @@ const bcrypt = require("bcrypt");
 
 (async () => {
   const senhaDigitada = "123456";
-  const hashBanco =
-    "$2b$10$OlwrB7EerWqmdzbynFopM.fVLXk9OFIB2V/oyuccqYB2.wtLvMHi6";
+  const hashBanco = "123456";
 
   const resultado = await bcrypt.compare(senhaDigitada, hashBanco);
 
